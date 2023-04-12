4d60e15835a5899c2d0fdef8af7aef53113b3ea2
emumia <emukhan568@gmail.com>
2023-04-13 05:07:45 +0600

    first commit

---
+++ studymart/Deep_learning/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Deep_learning(request):
+    return render(request,'deep_learning/dl_page.html')
---
+++ studymart/machine_learning/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+def machine_learning(request):
+    return render(request,'machine_learning/ml_page.html')
